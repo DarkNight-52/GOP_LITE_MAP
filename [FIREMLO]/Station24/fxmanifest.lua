c12251be9730f444acf2692fc426cd28cc5cdcfa
DUkU430 <zyuan595@gmail.com>
2025-08-01 04:37:17 +0800

    f

---
+++ [FIREMLO]/Station24/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Xonix Development'
+description 'Station24'
+version '1.0.0'
+
+file 'richman.ymt'
+data_file 'SCENARIO_POINTS_OVERRIDE_FILE' 'richman.ymt'
+this_is_a_map 'yes'
---
+++ [FIREMLO]/station8/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Xonix Development'
+description 'Staion8'
+version '1.0.0'
+
+this_is_a_map 'yes'
