f53eb656ca0d9c8b897096b96337a42fd31f1d1e
DUkU430 <zyuan595@gmail.com>
2025-09-08 17:00:52 +0800

    LASD MAP

---
+++ [LEOMLO]/lasdwv/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "cerulean"
+game "gta5"
+
+author "sergeant_jhonny"
+description "LSSD West Vinewood Station"
+version "1.0"
+this_is_a_map "yes"
+
+files {
+	"sp_manifest.ymt",
+	"audio/sd_door_game.dat151.rel"
+}
+
+data_file "SCENARIO_POINTS_OVERRIDE_PSO_FILE" "sp_manifest.ymt"
+data_file "AUDIO_GAMEDATA" "audio/sd_door_game.dat"
