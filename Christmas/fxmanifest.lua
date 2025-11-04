bb08de679368bf50e431937181aab5a69f2324b7
TTL_CA <css920805@gmail.com>
2025-11-04 15:33:07 +0800

    christmas

---
+++ Christmas/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+author 'Razed'
+description 'This resource contains a variety of winter themed content around the map.'
+version '1.1.0'
+
+this_is_a_map 'yes'
