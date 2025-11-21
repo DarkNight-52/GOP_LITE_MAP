03bd9c805b72d2d6af00171f445852e76e63078d
TTL_CA <css920805@gmail.com>
2025-11-22 02:08:14 +0800

    upd banzbank

---
+++ [civrp]/cfx_como_uniongarage/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'Como Sans'
+description 'Union Garage Doors'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/unlocked/*.*'
+}
+
+dependency '/assetpacks'
+
+-- To Purchase more quality maps visit https://onlybanns.tebex.io/
+
+-- Any issues or want to learn more about Only Banns RP, join our discord https://discord.gg/onlybanns
+
+-- Looking to edit, fix, repair or optimize existing maps & other assets, send a message on discord : comosans
+
+dependency '/assetpacks'
