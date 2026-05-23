c5d6a8e880b8c4a883d3e01b8b602fff800cc1b8
DUkU430 <zyuan595@gmail.com>
2026-05-23 20:42:14 +0800

    609 apt

---
+++ [civrp]/cfx-draft-appart01/fxmanifest.lua
@@ -0,0 +1,19 @@
+lua54 'yes'
+fx_version 'cerulean'
+game 'gta5'
+
+
+
+author 'Draft Studio by d1n'
+description 'appart01'
+version '1.1.0'
+
+this_is_a_map 'yes'
+
+
+
+/*
+client_scripts {
+	'client.lua'
+}
+*/
